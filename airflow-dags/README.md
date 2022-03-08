e6397d4a61ccbb619825a12f406c849b687bd701
Alex Ott <alexott@gmail.com>
2022-03-08 12:54:12 +0100

    Start to add examples of Airflow DAGs

---
+++ airflow-dags/README.md
@@ -0,0 +1,23 @@
+# Airflow DAGs
+
+This directory contains examples of Airflow DAGs that use [apache-airflow-providers-databricks](https://airflow.apache.org/docs/apache-airflow-providers-databricks/stable/index.html).
+
+## Loading weather data into Databricks SQL Endpoint
+
+[load_weather_into_dbsql.py](load_weather_into_dbsql.py) contains an example of the Airflow DAG that loads a weather data for some cities into a Delta table using Databricks SQL Endpoint.  DAG consists of following steps:
+
+* `create_table` - creates a Delta table if it doesn't exist using `DatabricksSqlOperator`
+* `get_weather_data` - fetch weather data using calls to REST API and saves data to a local disk using `PythonOperator`
+* `upload_weather_data` - uploads data from local disk to Azure Blob Storage using `LocalFilesystemToWasbOperator`
+* `import_weather_data` - imports uploaded data with `COPY INTO` SQL command executed via `DatabricksCopyIntoOperator`.
+
+To make it working in your environment you need to change following constant:
+
+* `WASBS_CONN_ID` - name of a [Azure Blob Storage connection](https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/stable/connections/wasb.html).
+* `DATABRICKS_SQL_ENDPOINT_NAME` - name of a [Databricks SQL endpoint](https://docs.databricks.com/sql/admin/sql-endpoints.html) that will be used for creation of the table and importing of data.
+* `DATABRICKS_CONN_ID` - name of a [Databricks connection](https://airflow.apache.org/docs/apache-airflow-providers-databricks/stable/connections/databricks.html) that will be used for authentication to Databricks workspace.
+* `DESTINATION_TABLE_NAME` - name of Delta table that will be created & loaded with data.
+* `LANDING_LOCATION_PREFIX` - name of directory inside the ADLS container. 
+* `ADLS_CONTAINER_NAME` - name of ADLS container.
+* `ADLS_STORAGE_NAME` - name of ADLS storage account (without `.dfs.core.windows.net`).
+
---
+++ airflow-dags/load_weather_into_dbsql.py
@@ -0,0 +1,111 @@
+import json
+from typing import Optional
+from datetime import date
+
+import requests
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.databricks.operators.databricks_sql import (
+    DatabricksCopyIntoOperator,
+    DatabricksSqlOperator,
+)
+from airflow.providers.microsoft.azure.transfers.local_to_wasb import LocalFilesystemToWasbOperator
+from airflow.utils.dates import days_ago
+
+# constants to change
+WASBS_CONN_ID = 'wasbs-test'
+DATABRICKS_SQL_ENDPOINT_NAME = "Airflow Test"
+DATABRICKS_CONN_ID = "my-shard-pat"
+DESTINATION_TABLE_NAME = "default.airflow_weather"
+LANDING_LOCATION_PREFIX = "airflow/landing"
+ADLS_CONTAINER_NAME = "test"
+ADLS_STORAGE_NAME = "test"
+
+#
+cities = ["london", "berlin", "amsterdam"]
+BASE_API_URL = "https://weatherdbi.herokuapp.com/data/weather/"
+ADLS_LANDING_LOCATION = f"abfss://{ADLS_CONTAINER_NAME}@{ADLS_STORAGE_NAME}.dfs.core.windows.net/" \
+                        f"{LANDING_LOCATION_PREFIX}/ "
+
+
+airflow_default_args = {
+    "owner": "airflow",
+    "databricks_conn_id": DATABRICKS_CONN_ID,
+    "sql_endpoint_name": DATABRICKS_SQL_ENDPOINT_NAME,
+}
+
+
+def import_city_data(city: str):
+    def convert_percents_string(s: str) -> Optional[float]:
+        if s and s.endswith('%'):
+            return float(s[:-1]) / 100
+        return None
+
+    url = BASE_API_URL + city.lower()
+    req = requests.get(url)
+    data_json = req.json()
+    data = {'date': str(date.today()),
+            'region': data_json['region'],
+            'temperature': data_json['currentConditions']['temp']['c'],
+            'precipitation': convert_percents_string(data_json['currentConditions']['precip']),
+            'humidity': convert_percents_string(data_json['currentConditions']['humidity']),
+            'wind': data_json['currentConditions']['wind']['km'],
+            'condition': data_json['currentConditions']['comment'],
+            'next_days': [{'day': d['day'], 'condition': d['comment'],
+                           'max_temp': d['max_temp']['c'], 'min_temp': d['min_temp']['c']}
+                          for d in data_json.get('next_days', [])],
+            }
+    return data
+
+
+def get_weather_data(output_path: str):
+    data = [json.dumps(import_city_data(city)) + "\n" for city in cities]
+    with open(output_path, "w") as f:
+        f.writelines(data)
+
+
+with DAG(
+        "load_weather_into_dbsql",
+        start_date=days_ago(0),
+        schedule_interval="@daily",
+        default_args=airflow_default_args,
+        catchup=False,
+) as dag:
+    schema = "date date, condition STRING, humidity double, precipitation double, region STRING, " \
+             "temperature long, wind long, next_days ARRAY<STRUCT<condition: STRING, day: STRING, " \
+             "max_temp: long, min_temp: long>>"
+
+    create_table = DatabricksSqlOperator(
+        task_id="create_table",
+        sql=[f"create table if not exists {DESTINATION_TABLE_NAME}({schema}) using delta"],
+    )
+
+    get_weather_data = PythonOperator(task_id="get_weather_data",
+                                      python_callable=get_weather_data,
+                                      op_kwargs={
+                                          "output_path": "/tmp/{{next_ds}}.json"
+                                      },
+                                      )
+
+    copy_data_to_adls = LocalFilesystemToWasbOperator(
+        task_id='upload_weather_data',
+        wasb_conn_id=WASBS_CONN_ID,
+        file_path="/tmp/{{next_ds}}.json",
+        container_name='test',
+        blob_name= LANDING_LOCATION_PREFIX + "/{{next_ds}}.json",
+        load_options={"overwrite": True,},
+    )
+
+    # this works only on clusters, not on SQL Endpoints
+    import_weather_data = DatabricksCopyIntoOperator(
+        task_id="import_weather_data",
+        expression_list="date::date, * except(date)",
+        table_name=DESTINATION_TABLE_NAME,
+        file_format="JSON",
+        file_location=ADLS_LANDING_LOCATION,
+        files=["{{next_ds}}.json"],
+        #validate=True, # this requires Preview channel
+        force_copy=True,
+    )
+
+    (create_table >> get_weather_data >> copy_data_to_adls >> import_weather_data)
