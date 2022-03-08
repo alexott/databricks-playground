9fff951674f8c19124090104b45f0b5871ffedb4
Alex Ott <alexott@gmail.com>
2022-03-08 15:40:00 +0100

    add more functions

--- pyspark-snippets/pyspark_helpers/columns_helpers.py
+++ pyspark-snippets/pyspark_helpers/columns_helpers.py
@@ -1,7 +1,20 @@
 from typing import List
 
 import pyspark.sql.functions as F
-from pyspark.sql import DataFrame
+from pyspark.sql import DataFrame, Column
+
+
+def columns_except(df: DataFrame, ex: List[str] = None) -> List[Column]:
+    """
+    Returns a new list of columns without specified columns
+    :param df: dataframe
+    :param ex: columns to exclude
+    :return: new dataframe
+    """
+    if ex is None:
+        ex = []
+
+    return [F.col(cl) for cl in df.columns if cl not in ex]
 
 
 def except_columns(df: DataFrame, ex: List[str] = None) -> DataFrame:
@@ -11,7 +24,4 @@ def except_columns(df: DataFrame, ex: List[str] = None) -> DataFrame:
     :param ex: columns to exclude
     :return: new dataframe
     """
-    if ex is None or len(ex) == 0:
-        return df
-
-    return df.select(*[F.col(cl) for cl in df.columns if cl not in ex])
+    return df.select(*columns_except(df, ex))
--- pyspark-snippets/pyspark_helpers/json_helpers.py
+++ pyspark-snippets/pyspark_helpers/json_helpers.py
@@ -3,10 +3,10 @@ from pyspark.sql import SparkSession
 
 
 def schema_of_json(line: str) -> str:
-  """
-  Returns a Spark DDL string
-  :param line: string with JSON data
-  :return: Spark DDL string
-  """
-  df = SparkSession.getActiveSession().range(1).select(F.schema_of_json(F.lit(line)))
-  return df.collect()[0][0]
+    """
+    Returns a Spark DDL string
+    :param line: string with JSON data
+    :return: Spark DDL string
+    """
+    df = SparkSession.getActiveSession().range(1).select(F.schema_of_json(F.lit(line)))
+    return df.collect()[0][0]
