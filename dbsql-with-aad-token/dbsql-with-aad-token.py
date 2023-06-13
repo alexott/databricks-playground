8c3343dbf98523bfe963edd3ea84141fc6bf635f
Alex Ott <alexott@gmail.com>
2023-06-13 13:31:23 +0200

    rewrite DBSQL AAD example to use auth from Python SDK

--- dbsql-with-aad-token/dbsql-with-aad-token.py
+++ dbsql-with-aad-token/dbsql-with-aad-token.py
@@ -1,5 +1,5 @@
 from databricks import sql
-from azure.identity import ClientSecretCredential
+from databricks.sdk.core import azure_service_principal, Config
 import os
 
 def get_env_or_raise(name: str) -> str:
@@ -9,34 +9,34 @@ def get_env_or_raise(name: str) -> str:
 
   return value
 
+# Variables to fill
+# Host name without https://
+host_name="adb-6688808130562317.17.azuredatabricks.net"
+# Path obtained as per instructions https://docs.databricks.com/dev-tools/python-sql-connector.html#get-started
+http_path="/sql/1.0/warehouses/dca3a0ba199040eb"
+# Your query to execute
+query = "select * from samples.nyctaxi.trips limit 5"
+
+
+# Explicit initialization of the authenticator
 tenant_id = get_env_or_raise("ARM_TENANT_ID")
 client_id = get_env_or_raise("ARM_CLIENT_ID")
 client_secret = get_env_or_raise("ARM_CLIENT_SECRET")
-csc = ClientSecretCredential(tenant_id, client_id, client_secret)
 
-dbx_scope = '2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default'
-aad_token = csc.get_token(dbx_scope).token
+creds = azure_service_principal(Config(azure_client_secret=client_secret, azure_client_id=client_id,
+                                       azure_tenant_id=tenant_id, host=f"https://{host_name}"))
 
-# Variables to fill
-# Host name without https://
-host_name=""
-# Path obtained as per instructions https://docs.databricks.com/dev-tools/python-sql-connector.html#get-started
-http_path=""
-# Your query to execute
-query = ""
+# Implicit initialization - you need to specify corresponding environment variables
+# creds = azure_service_principal(Config())
 
-connection = sql.connect(
-  server_hostname=host_name,
-  http_path=http_path,
-  access_token=aad_token)
+with sql.connect(server_hostname=host_name, http_path=http_path,
+                 credentials_provider=lambda: creds) as connection:
+  cursor = connection.cursor()
+  cursor.execute(query)
+  result = cursor.fetchall()
 
-cursor = connection.cursor()
+  for row in result:
+    print(row)
 
-cursor.execute(query)
-
-result = cursor.fetchall()
-
-for row in result:
-  print(row)
-
-cursor.close()
+  cursor.close()
+  connection.close()
