661636788e0c50eb0092fd786e7036544af574e4
Alex Ott <alexott@gmail.com>
2023-06-15 08:32:32 +0200

    Switch to use `logging` library instead of `print`

--- ip-access-list-analyzer/README.md
+++ ip-access-list-analyzer/README.md
@@ -25,7 +25,7 @@ To run the tool just execute:
 python ip-acl-analyzer.py [options]
 ```
 
-Pass `--help` command-line flag to obtain built-in help.
+Pass `--help` command-line flag to obtain built-in help.  Specify `--debug` option to get detailed log output.
 
 This tool works in two modes:
 
@@ -38,41 +38,41 @@ This tool works in two modes:
 If you execute following command:
 
 ```sh
-python ip-acl-analyzer.py --json-file=test.json
+python ip-acl-analyzer.py --json-file=test.json --debug
 ```
 
 Then you will receive following output:
 
 ```
-Performing only analysis...
-Going to load IP Access Lists from JSON file: test.json
-There are duplicates in the list! len(all_ips)=241, len(uniq_ips)=237
-Processing list 'list1' (0f209622-ca20-455a-bdc4-4de3bed8a1ed)
-	Found intersection with list list1 dup
-Modifying current list...
-	Removing from list1: ['54.81.134.249', '52.22.161.231', '52.45.144.63']
-Processing list 'list1 dup' (1f209622-ca20-455a-bdc4-4de3bed8a1ed)
-	Found intersection with list list2
-    Modifying current list...
-	52.55.144.63 is part of 52.55.144.0/24, removing...
-	Removing from list1 dup: ['52.55.144.63']
-Processing list 'list2' (1f209623-ca20-455a-bdc4-4de3bed8a1ed)
-	We can ignore 10.0.1.0 because it's local or private...
-	We can ignore 10.1.2.0/24 because it's local or private...
-	We can ignore 192.168.10.11 because it's local or private...
-	52.55.144.63 is part of 52.55.144.0/24, removing...
-	Removing from list2: ['192.168.10.11', '10.0.1.0', '10.1.2.0/24', '52.55.144.63']
-Processing list 'github_actions' (d798c5f5-3b53-4dc7-85b7-75dd67056512)
-Skipping not enabled list Disabled list (fc594781-60cb-4b46-b0f7-ee9d951e3c3f)
-Going to remove list 'list1' (0f209622-ca20-455a-bdc4-4de3bed8a1ed) as it's empty
-Going to modify list 'list1 dup' (1f209622-ca20-455a-bdc4-4de3bed8a1ed)
-	from: IpAccessListInfo(address_count=3, created_at=1651523910411, created_by=5381669867036714, enabled=True, ip_addresses=['52.45.144.63', '52.55.144.63', '54.81.134.249', '52.22.161.231'], label='list1 dup', list_id='1f209622-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=1651523910411, updated_by=5381669867036714)
-	to  : IpAccessListInfo(address_count=3, created_at=None, created_by=None, enabled=True, ip_addresses=['52.45.144.63', '54.81.134.249', '52.22.161.231'], label='list1 dup', list_id='1f209622-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=None, updated_by=None)
-Going to modify list 'list2' (1f209623-ca20-455a-bdc4-4de3bed8a1ed)
-	from: IpAccessListInfo(address_count=7, created_at=1651523910411, created_by=5381669867036714, enabled=True, ip_addresses=['52.55.144.63', '52.55.144.0/24', '54.91.134.249', '52.12.161.231', '10.0.1.0', '10.1.2.0/24', '192.168.10.11'], label='list2', list_id='1f209623-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=1651523910411, updated_by=5381669867036714)
-	to  : IpAccessListInfo(address_count=3, created_at=None, created_by=None, enabled=True, ip_addresses=['52.55.144.0/24', '54.91.134.249', '52.12.161.231'], label='list2', list_id='1f209623-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=None, updated_by=None)
-List 'github_actions' (d798c5f5-3b53-4dc7-85b7-75dd67056512) isn't modified or not enabled
-List 'Disabled list' (fc594781-60cb-4b46-b0f7-ee9d951e3c3f) isn't modified or not enabled
+DEBUG:root:Performing only analysis...
+DEBUG:root:Going to load IP Access Lists from JSON file: test.json
+INFO:root:There are duplicates in the IP Access lists! len(all_ips)=241, len(uniq_ips)=237
+DEBUG:root:Processing list 'list1' (0f209622-ca20-455a-bdc4-4de3bed8a1ed)
+DEBUG:root:	Found intersection with list list1 dup
+DEBUG:root:	Modifying current list...
+DEBUG:root:	Removing from list1: ['54.81.134.249', '52.22.161.231', '52.45.144.63']
+DEBUG:root:Processing list 'list1 dup' (1f209622-ca20-455a-bdc4-4de3bed8a1ed)
+DEBUG:root:	Found intersection with list list2
+DEBUG:root:	Modifying current list...
+DEBUG:root:	52.55.144.63 is part of 52.55.144.0/24, removing...
+DEBUG:root:	Removing from list1 dup: ['52.55.144.63']
+DEBUG:root:Processing list 'list2' (1f209623-ca20-455a-bdc4-4de3bed8a1ed)
+DEBUG:root:	We can ignore 10.0.1.0 because it's local or private...
+DEBUG:root:	We can ignore 10.1.2.0/24 because it's local or private...
+DEBUG:root:	We can ignore 192.168.10.11 because it's local or private...
+DEBUG:root:	52.55.144.63 is part of 52.55.144.0/24, removing...
+DEBUG:root:	Removing from list2: ['192.168.10.11', '10.0.1.0', '10.1.2.0/24', '52.55.144.63']
+DEBUG:root:Processing list 'github_actions' (d798c5f5-3b53-4dc7-85b7-75dd67056512)
+DEBUG:root:Skipping not enabled list Disabled list (fc594781-60cb-4b46-b0f7-ee9d951e3c3f)
+INFO:root:Going to remove list 'list1' (0f209622-ca20-455a-bdc4-4de3bed8a1ed) as it's empty
+INFO:root:Going to modify list 'list1 dup' (1f209622-ca20-455a-bdc4-4de3bed8a1ed). Entries to remove: ['52.55.144.63']
+DEBUG:root:	from: IpAccessListInfo(address_count=3, created_at=1651523910411, created_by=5381669867036714, enabled=True, ip_addresses=['52.45.144.63', '52.55.144.63', '54.81.134.249', '52.22.161.231'], label='list1 dup', list_id='1f209622-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=1651523910411, updated_by=5381669867036714)
+DEBUG:root:	to  : IpAccessListInfo(address_count=3, created_at=None, created_by=None, enabled=True, ip_addresses=['52.45.144.63', '54.81.134.249', '52.22.161.231'], label='list1 dup', list_id='1f209622-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=None, updated_by=None)
+INFO:root:Going to modify list 'list2' (1f209623-ca20-455a-bdc4-4de3bed8a1ed). Entries to remove: ['192.168.10.11', '10.0.1.0', '10.1.2.0/24', '52.55.144.63']
+DEBUG:root:	from: IpAccessListInfo(address_count=7, created_at=1651523910411, created_by=5381669867036714, enabled=True, ip_addresses=['52.55.144.63', '52.55.144.0/24', '54.91.134.249', '52.12.161.231', '10.0.1.0', '10.1.2.0/24', '192.168.10.11'], label='list2', list_id='1f209623-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=1651523910411, updated_by=5381669867036714)
+DEBUG:root:	to  : IpAccessListInfo(address_count=3, created_at=None, created_by=None, enabled=True, ip_addresses=['52.55.144.0/24', '54.91.134.249', '52.12.161.231'], label='list2', list_id='1f209623-ca20-455a-bdc4-4de3bed8a1ed', list_type=<ListType.ALLOW: 'ALLOW'>, updated_at=None, updated_by=None)
+INFO:root:List 'github_actions' (d798c5f5-3b53-4dc7-85b7-75dd67056512) isn't modified or not enabled
+INFO:root:List 'Disabled list' (fc594781-60cb-4b46-b0f7-ee9d951e3c3f) isn't modified or not enabled
 ```
 
 Based on the output we can see that following changes will be done:
--- ip-access-list-analyzer/ip-acl-analyzer.py
+++ ip-access-list-analyzer/ip-acl-analyzer.py
@@ -3,6 +3,7 @@ from typing import List
 import copy
 import itertools
 import json
+import logging
 
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.settings import IpAccessListInfo, ListType
@@ -20,10 +21,10 @@ def check_for_subnet_inclusion(ip_addresses: List[str], oip: str, cond: bool, to
             continue
         if ip.find("/") == -1:
             if ipaddress.ip_address(ip) in oip_net and cond:
-                print(f"\t{ip} is part of {oip}, removing...")
+                logging.debug(f"\t{ip} is part of {oip}, removing...")
                 to_remove.append(ip)
         elif oip_net.supernet_of(ipaddress.ip_network(ip, False)) and cond:
-            print(f"\t{ip} is subnet of {oip}, removing...")
+            logging.debug(f"\t{ip} is subnet of {oip}, removing...")
             to_remove.append(ip)
 
 
@@ -38,22 +39,22 @@ def analyze_lists(orig: List[IpAccessListInfo]) -> List[IpAccessListInfo]:
     all_ips = list(itertools.chain(*[l.ip_addresses for l in lsts]))
     uniq_ips = set(all_ips)
     if len(all_ips) != len(uniq_ips):
-        print(f"There are duplicates in the list! len(all_ips)={len(all_ips)}, len(uniq_ips)={len(uniq_ips)}")
+        logging.info(f"There are duplicates in the IP Access lists! len(all_ips)={len(all_ips)}, len(uniq_ips)={len(uniq_ips)}")
 
     for i in range(len(lsts)):
         l = lsts[i]
         if not l.enabled:
-            print(f"Skipping not enabled list {l.label} ({l.list_id})")
+            logging.debug(f"Skipping not enabled list {l.label} ({l.list_id})")
             continue
-        print(f"Processing list '{l.label}' ({l.list_id})")
+        logging.debug(f"Processing list '{l.label}' ({l.list_id})")
         to_remove = []
         ip_addresses = l.ip_addresses
         # find duplicates inside the list
         if len(set(ip_addresses)) != len(ip_addresses):
-            print("\tThere are duplicates in IP list, removing")
-            print(f"\tOld list: {ip_addresses}")
+            logging.debug("\tThere are duplicates in IP list, removing")
+            #logging.debug(f"\tOld list: {ip_addresses}")
             ip_addresses = list(set(ip_addresses))
-            print(f"\tNew list: {ip_addresses}")
+            #logging.debug(f"\tNew list: {ip_addresses}")
 
         # find duplicates & overlaps with other lists
         for l2 in lsts:
@@ -62,9 +63,9 @@ def analyze_lists(orig: List[IpAccessListInfo]) -> List[IpAccessListInfo]:
                 continue
             dups = set(ip_addresses).intersection(l2.ip_addresses)
             if len(dups) > 0:
-                print(f"\tFound intersection with list {l2.label}")
+                logging.debug(f"\tFound intersection with list {l2.label}")
                 if l2.list_type == ListType.BLOCK or (l.list_type == ListType.ALLOW):
-                    print("\tModifying current list...")
+                    logging.debug("\tModifying current list...")
                     to_remove.extend(dups)
 
             # finding subranges
@@ -73,7 +74,7 @@ def analyze_lists(orig: List[IpAccessListInfo]) -> List[IpAccessListInfo]:
                     check_for_subnet_inclusion(ip_addresses, oip,
                                                l2.list_type == l.list_type, to_remove)
                 except ValueError as ex:
-                    print(f"\tWarn: Incorrect IP Address or Network: '{ex}'")
+                    logging.warn(f"\tWarn: Incorrect IP Address or Network: '{ex}'")
 
         # Find private & local IPs
         for ip in ip_addresses:
@@ -83,20 +84,20 @@ def analyze_lists(orig: List[IpAccessListInfo]) -> List[IpAccessListInfo]:
                 else:
                     ip_addr = ipaddress.ip_network(ip, False)
                 if ip_addr.is_loopback or ip_addr.is_private:
-                    print(f"\tWe can ignore {ip} because it's local or private...")
+                    logging.debug(f"\tWe can ignore {ip} because it's local or private...")
                     to_remove.append(ip)
             except ValueError as ex:
-                print(f"\tWarn: Incorrect IP Address or Network: '{ip}': {ex}")
+                logging.warn(f"\tWarn: Incorrect IP Address or Network: '{ip}': {ex}")
 
         # find subranges, in the current list as well...
         for oip in ip_addresses:
             try:
                 check_for_subnet_inclusion(ip_addresses, oip, True, to_remove)
             except ValueError as ex:
-                print(f"\tWarn: Incorrect IP Address or Network: '{ex}'")
+                logging.warn(f"\tWarn: Incorrect IP Address or Network: '{ex}'")
 
         if len(to_remove) > 0:
-            print(f"\tRemoving from {l.label}: {list(set(to_remove))}")
+            logging.debug(f"\tRemoving from {l.label}: {list(set(to_remove))}")
             for ip in set(to_remove):
                 ip_addresses.remove(ip)
 
@@ -121,17 +122,18 @@ def apply_modifications(w: WorkspaceClient, make_changes: bool, orig: List[IpAcc
     ol = dict([(l.list_id, l) for l in orig])
     for l in new:
         if len(l.ip_addresses) == 0:
-            print(f"Going to remove list '{l.label}' ({l.list_id}) as it's empty")
+            logging.info(f"Going to remove list '{l.label}' ({l.list_id}) as it's empty")
             if w and make_changes:
                 w.ip_access_lists.delete(l.list_id)
             continue
         old = ol[l.list_id]
         if len(l.ip_addresses) == len(old.ip_addresses) or not l.enabled:
-            print(f"List '{l.label}' ({l.list_id}) isn't modified or not enabled")
+            logging.info(f"List '{l.label}' ({l.list_id}) isn't modified or not enabled")
         else:
-            print(f"Going to modify list '{l.label}' ({l.list_id})")
-            print(f"\tfrom: {old}")
-            print(f"\tto  : {l}")
+            diff = list(set(old.ip_addresses).difference(set(l.ip_addresses)))
+            logging.info(f"Going to modify list '{l.label}' ({l.list_id}). Entries to remove: {diff}")
+            logging.debug(f"\tfrom: {old}")
+            logging.debug(f"\tto  : {l}")
             if w and make_changes:
                 w.ip_access_lists.update(label=l.label, list_id=l.list_id, list_type=l.list_type,
                                          enabled=l.enabled, ip_addresses=l.ip_addresses)
@@ -141,26 +143,31 @@ def main():
     parser = argparse.ArgumentParser(description='Analyze and fix Databricks IP Access Lists')
     parser.add_argument('--apply', help="Do analysis and apply changes",
                         action='store_true', default=False)
+    parser.add_argument('--debug', help="Allow debug output",
+                        action='store_true', default=False)
     parser.add_argument('--json-file', nargs=1,
                         help="Load IP Access Lists from a JSON file that is output of lists API")
     args = parser.parse_args()
-    # print(args)
+    log_level = logging.INFO
+    if args.debug:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level)
     if args.apply:
-        print("Performing analysis & fixing issues...")
+        logging.debug("Performing analysis & fixing issues...")
     else:
-        print("Performing only analysis...")
+        logging.debug("Performing only analysis...")
 
     w = None
 
     if args.json_file:
-        print(f"Going to load IP Access Lists from JSON file: {args.json_file[0]}")
+        logging.debug(f"Going to load IP Access Lists from JSON file: {args.json_file[0]}")
         with open(args.json_file[0]) as f:
             d = json.load(f)
             ipls = [IpAccessListInfo.from_dict(l) for l in d['ip_access_lists']]
     else:
-        print("Getting IP Access Lists from workspace")
+        logging.debug("Getting IP Access Lists from workspace")
         w = WorkspaceClient()
-        print(f"Processing IP Access Lists for host {w.config.host}")
+        logging.debug(f"Processing IP Access Lists for host {w.config.host}")
         ipls = list(w.ip_access_lists.list())
 
     new_ipls = analyze_lists(ipls)
