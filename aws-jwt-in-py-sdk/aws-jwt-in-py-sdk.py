deb60d970ae5883c11507dc24496ab6458df2091
Alex Ott <alexott@gmail.com>
2026-02-11 13:44:00 +0100

    Pass audience explicitly

--- aws-jwt-in-py-sdk/aws-jwt-in-py-sdk.py
+++ aws-jwt-in-py-sdk/aws-jwt-in-py-sdk.py
@@ -18,7 +18,7 @@ tags = [
 
 # TODO: think how to pass these parameters to the constructor
 class AwsJwtIdTokenSource(oidc.IdTokenSource):
-    def __init__(self):
+    def __init__(self, audience: str):
         self.audience = audience
         self.duration = duration
         self.signing_algorithm = signing_algorithm
@@ -38,16 +38,19 @@ class AwsJwtIdTokenSource(oidc.IdTokenSource):
         return oidc.IdToken(jwt=token)
 
 
-@credentials_strategy("aws-jwt", [])
+@credentials_strategy("aws-jwt", ["token_audience"])
 def aws_jwt_strategy(cfg: Config) -> CredentialsProvider:
-    return oidc_credentials_provider(cfg, AwsJwtIdTokenSource())
+    return oidc_credentials_provider(cfg, AwsJwtIdTokenSource(audience=cfg.token_audience))
 
 
 host = os.getenv( "DATABRICKS_HOST") or "https://adb-.....17.azuredatabricks.net"
 client_id=os.getenv("CLIENT_ID") or "..."
 
-wc = WorkspaceClient(config=Config(host=host, client_id=client_id, 
-                                   credentials_strategy=aws_jwt_strategy))
+cfg=Config(host=host, 
+           client_id=client_id, 
+           credentials_strategy=aws_jwt_strategy, 
+           token_audience=audience)
+wc = WorkspaceClient(config=cfg)
 
 for cluster in wc.clusters.list():
     print(cluster.cluster_id, cluster.cluster_name)
